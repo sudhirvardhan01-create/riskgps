65c45435a769578cae36bb3e84dbf41d77edb2c5
shubhamGrey <sk101294@gmail.com>
2025-07-23 13:13:28 +0530

    Fixed failing test cases

--- frontend/src/components/Footer/index.test.tsx
+++ frontend/src/components/Footer/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Footer from ".";
+import Footer from "./index";
 import "@testing-library/jest-dom";
 
 describe("Footer", () => {
