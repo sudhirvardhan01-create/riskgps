a751e36face6f2f39a8226a35cb408c1cf1846e2
shubhamGrey <sk101294@gmail.com>
2025-07-30 13:31:56 +0530

    Fixed user sign up issue

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,9 +4,9 @@ module.exports = (sequelize, DataTypes) => {
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
     phone: { type: DataTypes.STRING, allowNull: true },
-    organisation: { type: DataTypes.STRING, allowNull: true },
+    organisation: { type: DataTypes.STRING, allowNull: false },
     message: { type: DataTypes.TEXT, allowNull: true },
-    communicationPreference: { type: DataTypes.ENUM('Email', 'Phone', 'Both'), allowNull: true }
+    communicationPreference: { type: DataTypes.ENUM('Email', 'Phone', 'Both'), allowNull: false }
   });
   User.associate = models => {
     User.belongsTo(models.Role, { foreignKey: 'roleId' });
