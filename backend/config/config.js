cdccc8d323a21fceac4fee64e40edbda9fc8effa
Harsh Kansal <harshkansal0711@gmail.com>
2025-08-08 15:17:04 +0530

    Comment some dialect related lines in backend/config/config.js, change meta-data to metadata in Sidebar/index.tsx and reduce the autoHideDuration to 3000 in ToastComponent.tsx

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 module.exports = {
   development: {
     use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: false,
-        rejectUnauthorized: false,
-      },
-    },
+    // dialect: "postgres",
+    // dialectOptions: {
+    //   ssl: {
+    //     require: false,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
   },
   production: {
     use_env_variable: "DATABASE_URL",
