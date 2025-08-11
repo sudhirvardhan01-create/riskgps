7588e949c4e801c85751092dd8dfed27351e5b50
Harsh Kansal <harshkansal0711@gmail.com>
2025-08-11 11:14:42 +0530

    Change the folder name to Library in components folder

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     use_env_variable: "DATABASE_URL",
-    // dialect: "postgres",
+    //dialect: "postgres",
     // dialectOptions: {
     //   ssl: {
     //     require: false,
