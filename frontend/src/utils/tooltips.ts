830aa3506acf7352f40608f7adf1cacff98663e9
Harsh Kansal <harshkansal0711@gmail.com>
2025-08-13 22:07:24 +0530

    Add some key and value pairs in tooltips.ts

--- frontend/src/utils/tooltips.ts
+++ frontend/src/utils/tooltips.ts
@@ -1,4 +1,6 @@
 export const tooltips = {
     //Risk Scenario 
     riskScenario: "Enter the Risk Scenario",
+    riskStatement: "Enter the Risk Statement",
+    riskDescription: "Enter the Risk Description",
 }
