3d5a57fe06d1fc98606a801a20845c31626d9958
Harsh Kansal <harshkansal0711@gmail.com>
2025-10-06 23:09:48 +0530

    Change some files and folders names

---
+++ backend/seeders/seed-load.js
@@ -0,0 +1,13 @@
+//include below files in sequence
+
+//sedd-roles-and-admins.js
+
+//seed-metadata.js
+//seed-process.js
+//seed-risk-scenario.js
+//seed-assets.js
+//seed-mitre-threats-controls.js
+//seed-nist-controls.js
+//seed-threat-bundle.js
+
+//exclude below files
