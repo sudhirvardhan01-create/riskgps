0143fc8603b371552f89513524cb145ad2eef9a4
hakumar-madihum <harsh.kumar@madihum.com>
2025-10-22 14:47:48 +0530

    Organization business unit apis

--- backend/models/organization/organization_business_unit.js
+++ backend/models/organization/organization_business_unit.js
@@ -1,55 +1,75 @@
-const commonFields = require("../common_fields");
+﻿const commonFields = require("../common_fields");
 
 module.exports = (sequelize, DataTypes) => {
-  const OrganizationBusinessUnit = sequelize.define(
-    "OrganizationBusinessUnit",
-    {
-      orgBusinessUnitId: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-        field: "org_business_unit_id",
-      },
-      organizationId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        field: "org_id", // DB column, alias remains organizationId
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        field: "name",
-      },
-      desc: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "desc",
-      },
-      ...commonFields, // adds createdBy, modifiedBy, createdDate, modifiedDate, isDeleted
-    },
-    {
-      tableName: "organization_business_unit",
-      timestamps: false, // we manage timestamps manually
-    }
-  );
+    const OrganizationBusinessUnit = sequelize.define(
+        "OrganizationBusinessUnit",
+        {
+            orgBusinessUnitId: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                defaultValue: DataTypes.UUIDV4,
+                field: "org_business_unit_id",
+            },
+            organizationId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: "org_id",
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "name",
+            },
+            head: {
+                type: DataTypes.JSONB, // stores { name, email }
+                allowNull: true,
+                field: "head",
+            },
+            pocBiso: {
+                type: DataTypes.JSONB, // stores { name, email }
+                allowNull: true,
+                field: "poc_biso",
+            },
+            itPoc: {
+                type: DataTypes.JSONB, // stores { name, email }
+                allowNull: true,
+                field: "it_poc",
+            },
+            financeLead: {
+                type: DataTypes.JSONB, // stores { name, email }
+                allowNull: true,
+                field: "finance_lead",
+            },
+            tags: {
+                type: DataTypes.JSONB, // stores [{ key, value }, ...]
+                allowNull: true,
+                field: "tags",
+            },
+            ...commonFields, // createdBy, modifiedBy, createdDate, modifiedDate, isDeleted
+        },
+        {
+            tableName: "organization_business_unit",
+            timestamps: false,
+        }
+    );
 
-  OrganizationBusinessUnit.associate = (models) => {
-    // belongsTo Organization
-    OrganizationBusinessUnit.belongsTo(models.Organization, {
-      foreignKey: "organizationId", // alias, maps to org_id
-      targetKey: "organizationId",
-      as: "organizationDetails",
-    });
+    OrganizationBusinessUnit.associate = (models) => {
+        // Belongs to Organization
+        OrganizationBusinessUnit.belongsTo(models.Organization, {
+            foreignKey: "organizationId",
+            targetKey: "organizationId",
+            as: "organizationDetails",
+        });
 
-    // One Organization -> Many Business Units
-    if (models.Organization) {
-      models.Organization.hasMany(OrganizationBusinessUnit, {
-        foreignKey: "organizationId", // alias
-        sourceKey: "organizationId",
-        as: "organizationBusinessUnits",
-      });
-    }
-  };
+        // One Organization -> Many Business Units
+        if (models.Organization) {
+            models.Organization.hasMany(OrganizationBusinessUnit, {
+                foreignKey: "organizationId",
+                sourceKey: "organizationId",
+                as: "organizationBusinessUnits",
+            });
+        }
+    };
 
-  return OrganizationBusinessUnit;
+    return OrganizationBusinessUnit;
 };
