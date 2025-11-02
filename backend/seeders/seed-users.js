f0bc646277b46468451c1a7bc57a17fe0c569edb
Harsh Kansal <harshkansal0711@gmail.com>
2025-11-02 22:35:39 +0530

    Modify seed user file

--- backend/seeders/seed-users.js
+++ backend/seeders/seed-users.js
@@ -4,7 +4,10 @@ const { safeSeed } = require("../utils/seedHelper");
 
 module.exports = {
   up: async () => {
-    const hashedPassword = await bcrypt.hash("Admin@123", 10);
+    const hashedPassword1 = await bcrypt.hash("Admin@123", 10);
+    const hashedPassword2 = await bcrypt.hash("Vishal@123", 10);
+    const hashedPassword3 = await bcrypt.hash("Priti@123", 10);
+    const hashedPassword4 = await bcrypt.hash("abc12345", 10);
 
     const role = await Role.findOne({
       where: { name: "Admin" },
@@ -14,9 +17,39 @@ module.exports = {
       {
         name: "admin",
         email: "admin@bluoceancyber.com",
-        password: hashedPassword,
+        password: hashedPassword1,
         phone: "1234567890",
         organisation: "Admin Org",
+        message: "Welcome to the admin dashboard",
+        communicationPreference: "Email",
+        roleId: role.roleId,
+      },
+      {
+        name: "Vishal Chawla",
+        email: "vishal@bluoceancyber.com",
+        password: hashedPassword2,
+        phone: "1234567891",
+        organisation: "BluOcean Cyber",
+        message: "Welcome to the admin dashboard",
+        communicationPreference: "Email",
+        roleId: role.roleId,
+      },
+      {
+        name: "Priti Patil",
+        email: "priti.patil@bluoceancyber.com",
+        password: hashedPassword3,
+        phone: "1234567892",
+        organisation: "BluOcean Cyber",
+        message: "Welcome to the admin dashboard",
+        communicationPreference: "Email",
+        roleId: role.roleId,
+      },
+      {
+        name: "Prabhat Jha",
+        email: "prabhat.jha@bluoceancyber.com",
+        password: hashedPassword4,
+        phone: "1234567893",
+        organisation: "BluOcean Cyber",
         message: "Welcome to the admin dashboard",
         communicationPreference: "Email",
         roleId: role.roleId,
