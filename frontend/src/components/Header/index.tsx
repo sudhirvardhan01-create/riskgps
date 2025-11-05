5f6c3e0677c1f51818e108a3e587bf0853962040
Harsh Kansal <harshkansal0711@gmail.com>
2025-11-05 22:17:38 +0530

    Modify Landing Page and add picture on login page

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,14 @@
-import { Box, IconButton, Menu, MenuItem, Stack } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Stack,
+  Tooltip,
+} from "@mui/material";
 import React from "react";
 import NotificationIcon from "@/icons/notification.svg";
 import AvatarIcon from "@/icons/avatar.svg";
@@ -8,10 +18,13 @@ import { logout } from "@/pages/api/AuthAPI";
 import Cookies from "js-cookie";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
+import { Logout } from "@mui/icons-material";
 
 const Header = () => {
   const router = useRouter();
   const { logoutContext } = useAuth();
+  const userCookie = Cookies.get("user");
+  const user = userCookie ? JSON.parse(userCookie) : null;
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -41,24 +54,38 @@ const Header = () => {
     >
       <Image src={riskgps_logo} alt="RiskGPS Logo" width={150} height={36} />
       <Stack display={"flex"} flexDirection={"row"}>
-        <NotificationIcon
-          height={24}
-          width={24}
-          style={{ marginRight: 32, cursor: "pointer" }}
-        />
-        <IconButton
-          onClick={handleClick}
-          sx={{ width: 24, height: 24, padding: "0px !important" }}
-        >
-          <AvatarIcon height={24} width={24} style={{ cursor: "pointer" }} />
-        </IconButton>
+        <Tooltip title="Notifications">
+          <NotificationIcon
+            height={24}
+            width={24}
+            style={{ marginRight: 32, cursor: "pointer" }}
+          />
+        </Tooltip>
+        <Tooltip title="My Profile">
+          <IconButton
+            onClick={handleClick}
+            sx={{ width: 24, height: 24, padding: "0px !important" }}
+          >
+            <AvatarIcon height={24} width={24} style={{ cursor: "pointer" }} />
+          </IconButton>
+        </Tooltip>
         <Menu
           id="profile-menu"
           anchorEl={anchorEl}
           open={open}
           onClose={() => setAnchorEl(null)}
         >
-          <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
+          <MenuItem sx={{ cursor: "default" }}>
+            <Avatar /> &nbsp;
+            {user?.name as string}
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={() => handleLogout()}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
         </Menu>
       </Stack>
     </Box>
