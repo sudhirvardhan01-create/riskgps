e0e22036814d81d4bea74570f5d40dba2ce7b96e
hakumar-madihum <harsh.kumar@madihum.com>
2025-11-11 21:00:09 +0530

    fix: assessment api changes

--- backend/models/assessment/assessment.js
+++ backend/models/assessment/assessment.js
@@ -1,112 +1,118 @@
 ﻿const commonFields = require("../common_fields");
 
 module.exports = (sequelize, DataTypes) => {
-  const Assessment = sequelize.define(
-    "Assessment",
-    {
-      assessmentId: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-        field: "assessment_id",
-      },
-      assessmentName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        field: "assessment_name",
-      },
-      assessmentDesc: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "assessment_desc",
-      },
-      runId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        field: "run_id",
-      },
-      orgId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        field: "org_id",
-      },
-      orgName: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "org_name",
-      },
-      orgDesc: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "org_desc",
-      },
-      businessUnitId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        field: "business_unit_id",
-      },
-      businessUnitName: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "business_unit_name",
-      },
-      businessUnitDesc: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "business_unit_desc",
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        field: "status",
-      },
-      progress: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        field: "progress",
-      },
-      startDate: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        field: "start_date",
-      },
-      endDate: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        field: "end_date",
-      },
-      lastActivity: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        field: "last_activity",
-      },
-      ...commonFields, // includes createdBy, modifiedBy, createdDate, modifiedDate, isDeleted, tenantId, status, etc.
-    },
-    {
-      tableName: "assessment",
-      schema: "public",
-      timestamps: false,
-    }
-  );
+    const Assessment = sequelize.define(
+        "Assessment",
+        {
+            assessmentId: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                defaultValue: DataTypes.UUIDV4,
+                field: "assessment_id",
+            },
+            assessmentName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "assessment_name",
+            },
+            assessmentDesc: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: "assessment_desc",
+            },
+            runId: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "run_id",
+            },
+            orgId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: "org_id",
+            },
+            orgName: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: "org_name",
+            },
+            orgDesc: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: "org_desc",
+            },
+            businessUnitId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: "business_unit_id",
+            },
+            businessUnitName: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: "business_unit_name",
+            },
+            businessUnitDesc: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: "business_unit_desc",
+            },
+            status: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: "status",
+            },
+            progress: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+                field: "progress",
+            },
+            startDate: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: "start_date",
+            },
+            endDate: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: "end_date",
+            },
+            lastActivity: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: "last_activity",
+            },
+            ...commonFields, // includes createdBy, modifiedBy, createdDate, modifiedDate, isDeleted, tenantId, status, etc.
+        },
+        {
+            tableName: "assessment",
+            schema: "public",
+            timestamps: false,
+        }
+    );
 
-  Assessment.associate = (models) => {
-    // Organization Relation
-    Assessment.belongsTo(models.Organization, {
-      foreignKey: "orgId",
-      as: "organization",
-    });
+    Assessment.associate = (models) => {
+        // Organization Relation
+        Assessment.belongsTo(models.Organization, {
+            foreignKey: "orgId",
+            as: "organization",
+        });
 
-    // Business Unit Relation
-    Assessment.belongsTo(models.OrganizationBusinessUnit, {
-      foreignKey: "businessUnitId",
-      as: "businessUnit",
-    });
+        // Business Unit Relation
+        Assessment.belongsTo(models.OrganizationBusinessUnit, {
+            foreignKey: "businessUnitId",
+            as: "businessUnit",
+        });
 
-    Assessment.hasMany(models.AssessmentProcess, {
-      foreignKey: "assessmentId",
-      as: "processes",
-    });
-  };
+        Assessment.hasMany(models.AssessmentProcess, {
+            foreignKey: "assessmentId",
+            as: "processes",
+        });
 
-  return Assessment;
+        //Assessment.belongsTo(models.User, {
+        //    foreignKey: "createdBy",      // This maps to the createdBy column in Assessment
+        //    targetKey: "userId",          // This matches the userId in User model
+        //    as: "users",
+        //});
+    };
+
+    return Assessment;
 };
